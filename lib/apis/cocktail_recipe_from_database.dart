9cd02c361922dcb10d0f93bcb2c99a2716a5fb89
KZRT <kzrt0123@cau.ac.kr>
2023-04-13 02:42:14 +0900

    Add postgresql support
    Changed pubspec to add postgresql dependencies
    Add database_helper.dart for basic SQL handling
    Add cocktail_recipe_from_database.dart to note how to use database
    
    TODO: Make table spec for database handling

---
+++ lib/apis/cocktail_recipe_from_database.dart
@@ -0,0 +1,21 @@
+// From postgreSQL database grab cocktail recipe table
+
+// Path: lib/apis/cocktail_recipe_from_database.dart
+
+// Language: dart
+
+import 'package:moonshine_fe/apis/database_helper.dart';
+
+class CocktailDatabase{
+  var dbHelper = DatabaseHelper();
+
+  Future<List<Map<String, dynamic>>> getRecipeList() async{
+    var result = await dbHelper.getRecipeList();
+    return result;
+  }
+
+  Future<List<Map<String, dynamic>>> searchRecipe(String name) async{
+    var result = await dbHelper.searchRecipe(name);
+    return result;
+  }
+}
