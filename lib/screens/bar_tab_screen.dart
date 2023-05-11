ecc3887c9bb728e77ea25cbb58e0acfa33e3e419
robinjoo1015 <robinjoo1015@naver.com>
2023-05-12 03:45:02 +0900

    Add search bar

--- lib/screens/bar_tab_screen.dart
+++ lib/screens/bar_tab_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moonshine_fe/apis/cocktail_project.dart';
 import 'package:moonshine_fe/apis/geolocation.dart';
 import 'package:moonshine_fe/widgets/bar_tab_item_widget.dart';
+import 'package:searchfield/searchfield.dart';
 
 class BarTabScreen extends StatefulWidget {
   final Geolocation geolocation;
@@ -34,6 +35,10 @@ class _BarTabScreenState extends State<BarTabScreen> {
       future: barList,
       builder: ((context, snapshot) {
         if (snapshot.hasData) {
+          List<String> names = [];
+          for (var item in snapshot.data!) {
+            names.add(item['name']!);
+          }
           return ListView.separated(
             scrollDirection: Axis.vertical,
             itemCount: snapshot.data!.length ~/ 2,
@@ -47,16 +52,89 @@ class _BarTabScreenState extends State<BarTabScreen> {
             itemBuilder: (context, index) {
               // print(index);
               if (index == 0) {
-                return SizedBox(
-                  height: 40,
-                  child: Column(
-                    children: [
-                      const Text('Search'),
-                      Text(
-                        (currentAddress != null) ? currentAddress! : '',
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Center(
+                            child: Text('추천순'),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 10,
+                            ),
+                            child: SearchField(
+                              itemHeight: 40,
+                              maxSuggestionsInViewPort: 8,
+                              focusNode: FocusNode(
+                                skipTraversal: true,
+                              ),
+                              suggestionState: Suggestion.hidden,
+                              suggestionAction: SuggestionAction.unfocus,
+                              scrollbarAlwaysVisible: false,
+                              searchInputDecoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(5),
+                                  ),
+                                ),
+                              ),
+                              suggestions: names
+                                  .map((name) => SearchFieldListItem(
+                                        name,
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 20,
+                                            vertical: 10,
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Text(name),
+                                            ],
+                                          ),
+                                        ),
+                                      ))
+                                  .toList(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Icon(
+                            Icons.search_outlined,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Center(
+                        child: Text(
+                          (currentAddress != null) ? currentAddress! : '',
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        BarTabItem(
+                          imgUrl: snapshot.data![0]['imgUrl']!,
+                          name: snapshot.data![0]['name']!,
+                        ),
+                        BarTabItem(
+                          imgUrl: snapshot.data![1]['imgUrl']!,
+                          name: snapshot.data![1]['name']!,
+                        ),
+                      ],
+                    )
+                  ],
                 );
               }
               return Row(
--- lib/screens/cocktail_tab_screen.dart
+++ lib/screens/cocktail_tab_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moonshine_fe/apis/cocktail_project.dart';
 import 'package:moonshine_fe/widgets/cocktail_tab_item_widget.dart';
+import 'package:searchfield/searchfield.dart';
 
 class CocktailTabScreen extends StatelessWidget {
   CocktailTabScreen({super.key});
@@ -13,7 +14,10 @@ class CocktailTabScreen extends StatelessWidget {
       future: cocktailList,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          // print(snapshot.data);
+          List<String> names = [];
+          for (var item in snapshot.data!) {
+            names.add(item['name']!);
+          }
           return ListView.separated(
             scrollDirection: Axis.vertical,
             itemCount: snapshot.data!.length ~/ 2,
@@ -27,10 +31,79 @@ class CocktailTabScreen extends StatelessWidget {
             itemBuilder: (context, index) {
               if (index == 0) {
                 return Column(
-                  children: const [
-                    SizedBox(
-                      height: 40,
-                      child: Text('Search'),
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Center(child: Text('추천순')),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 10,
+                            ),
+                            child: SearchField(
+                              itemHeight: 40,
+                              maxSuggestionsInViewPort: 8,
+                              focusNode: FocusNode(
+                                skipTraversal: true,
+                              ),
+                              suggestionState: Suggestion.hidden,
+                              suggestionAction: SuggestionAction.unfocus,
+                              scrollbarAlwaysVisible: false,
+                              searchInputDecoration: const InputDecoration(
+                                // icon: Icon(Icons.search_outlined),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(5),
+                                  ),
+                                ),
+                              ),
+                              suggestions: names
+                                  .map(
+                                    (name) => SearchFieldListItem(
+                                      name,
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 20,
+                                          vertical: 10,
+                                        ),
+                                        child: Row(
+                                          children: [
+                                            Text(name),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Icon(
+                            Icons.search_outlined,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CocktailTabItem(
+                          imgUrl: snapshot.data![0]['src']!,
+                          name: snapshot.data![0]['name']!,
+                        ),
+                        CocktailTabItem(
+                          imgUrl: snapshot.data![1]['src']!,
+                          name: snapshot.data![1]['name']!,
+                        ),
+                      ],
                     ),
                   ],
                 );
