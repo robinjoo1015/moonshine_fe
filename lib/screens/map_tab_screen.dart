1bc32fb6f27892630946ff415f9ef6514824deca
hanseongmin <5555565@naver.com>
2023-06-01 01:20:02 +0900

    Add map current button

--- lib/screens/map_tab_screen.dart
+++ lib/screens/map_tab_screen.dart
@@ -1,31 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-// class MapTabScreen extends StatelessWidget {
-//   const MapTabScreen({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: const GoogleMap(
-//         mapType: MapType.normal,
-//         initialCameraPosition: CameraPosition(
-//           target: LatLng(37.5035, 126.961),
-//           zoom: 14,
-//         ),
-//         // onMapCreated: ,
-//       ),
-//     );
-//   }
-
-// }
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-import '../map_src/locations.dart' as locations;
 
 class MapTabScreen extends StatefulWidget {
   const MapTabScreen({Key? key}) : super(key: key);
@@ -35,40 +11,47 @@ class MapTabScreen extends StatefulWidget {
 }
 
 class _MapTabScreenState extends State<MapTabScreen> {
-  final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
-      _markers.clear();
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
-          infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
-          ),
-        );
-        _markers[office.name] = marker;
-      }
-    });
+  List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _markers.add(Marker(
+        markerId: MarkerId("1"),
+        draggable: true,
+        onTap: () => print("Marker!"),
+        position: LatLng(37.4537251, 126.7960716)));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GoogleMap(
-        onMapCreated: _onMapCreated,
-        zoomGesturesEnabled: true,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(37.5035, 126.961),
-          zoom: 14,
-        ),
-        myLocationButtonEnabled: true,
-        markers: _markers.values.toSet(),
-        gestureRecognizers: Set()
-          ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer())),
+    // var markers;
+    return GoogleMap(
+      zoomGesturesEnabled: true,
+      zoomControlsEnabled: true,
+      mapType: MapType.normal,
+      markers: Set.from(_markers),
+      initialCameraPosition: const CameraPosition(
+        target: LatLng(37.5035, 126.961),
+        zoom: 14,
       ),
+      cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+        southwest: const LatLng(33, 124),
+        northeast: const LatLng(39, 132),
+      )), //카메라 이동 제한(대한민국으로 특정)
+      minMaxZoomPreference: MinMaxZoomPreference(10, 18), //최소, 최대 줌
+      myLocationButtonEnabled: true, //내 위치 버튼
+      myLocationEnabled: true, //내 위치
+      gestureRecognizers: Set() //제스처 인식
+        ..add(Factory<PanGestureRecognizer>(() => PanGestureRecognizer())),
+      //현재 위치 알려주는 버튼
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
+}
+
+class _controller {
+  static complete(GoogleMapController controller) {}
 }
