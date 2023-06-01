79e831c973e604ada2c6253e706f1b40f129009a
robinjoo1015 <robinjoo1015@naver.com>
2023-06-01 15:35:16 +0900

    Fix geolocation

--- lib/apis/geolocation.dart
+++ lib/apis/geolocation.dart
@@ -1,5 +1,7 @@
-import 'package:geocoding/geocoding.dart';
+import 'dart:convert';
+
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Geolocation {
   Position? _currentPosition;
@@ -11,7 +13,7 @@ class Geolocation {
     bool serviceEnabled;
     LocationPermission permission;
 
-    print('Geolocation _handleLocationPermission() executed');
+    // print('Geolocation _handleLocationPermission() executed');
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -39,7 +41,7 @@ class Geolocation {
   }
 
   Future<Position?> getCurrentPosition() async {
-    print('Geolocation getCurrentPosition() executed');
+    // print('Geolocation getCurrentPosition() executed');
 
     final hasPermission = await _handleLocationPermission();
 
@@ -49,40 +51,63 @@ class Geolocation {
       // setState(() => _currentPosition = position);
       _currentPosition = position;
       // _getAddressFromLatLng(_currentPosition!);
-      print(_currentPosition);
+      // print(_currentPosition);
       return _currentPosition;
     }).catchError((e) {
       // debugPrint(e);
-      print('_getCurrentPosition catchError');
+      // print('_getCurrentPosition catchError');
       print(e.toString());
     });
     return _currentPosition;
   }
 
   Future<String?> getAddressFromLatLng() async {
-    print('Geolocation getAddressFromLatLng() executed');
+    // print('Geolocation getAddressFromLatLng() executed');
 
     if (_currentPosition == null) {
       await getCurrentPosition();
     }
 
-    await placemarkFromCoordinates(
-            _currentPosition!.latitude, _currentPosition!.longitude)
-        .then((List<Placemark> placemarks) {
-      Placemark place = placemarks[0];
-      // setState(() {
-      //   _currentAddress =
-      //       '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
-      // });
+    // await placemarkFromCoordinates(
+    //         _currentPosition!.latitude, _currentPosition!.longitude)
+    //     .then((List<Placemark> placemarks) {
+    //   Placemark place = placemarks[0];
+    //   // setState(() {
+    //   //   _currentAddress =
+    //   //       '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+    //   // });
+    //   _currentAddress =
+    //       '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+    //   print(_currentAddress);
+    //   print(place.locality);
+    //   print(place.subThoroughfare);
+    //   print(place.thoroughfare);
+    //   print(place.administrativeArea);
+    //   print(place.name);
+    //   print(place.toString());
+    //   return _currentAddress;
+    // }).catchError((e) {
+    //   // debugPrint(e);
+    //   // print('_getAddressFromLatLng catchError');
+    //   print(e.toString());
+    // });
+
+    String url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${_currentPosition!.latitude},${_currentPosition!.longitude}&key=AIzaSyBhU7VhL4IZ2LOFrTw_C1rF2BqPbuj_olg&language=ko';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      // print(jsonDecode(response.body)['results'][1]['address_components'][3]
+      //     ['long_name']);
+      // print(jsonDecode(response.body)['results'][1]['address_components'][2]
+      //     ['long_name']);
+      var results = jsonDecode(response.body)['results'];
+
       _currentAddress =
-          '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
-      print(_currentAddress);
-      return _currentAddress;
-    }).catchError((e) {
-      // debugPrint(e);
-      print('_getAddressFromLatLng catchError');
-      print(e.toString());
-    });
+          '${results[1]['address_components'][3]['long_name']} ${results[1]['address_components'][2]['long_name']} ${results[1]['address_components'][1]['long_name']}';
+    } else {
+      _currentAddress = 'GPS Unavailable';
+    }
+    print(_currentAddress);
     return _currentAddress;
   }
 }
