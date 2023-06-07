a749b58d1d60017280737474f06d84da66f67053
robinjoo1015 <robinjoo1015@naver.com>
2023-06-08 01:38:37 +0900

    Login function

--- lib/screens/account_setting_screen.dart
+++ lib/screens/account_setting_screen.dart
@@ -1,9 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:moonshine_fe/config.dart' as globals;
+import 'package:http/http.dart' as http;
 
 class AccountSettingScreen extends StatelessWidget {
   const AccountSettingScreen({super.key});
+  static const baseUrl = 'http://3.135.207.29:3000';
+
+  Future<String?> _authUser(LoginData data) async {
+    // print('${data.name} ${data.password}');
+    // jsonEncode({'id': data.name, 'password': data.password});
+    print(jsonEncode({'name': data.name, 'password': data.password}));
+    var response = await http.post(Uri.parse(baseUrl),
+        body: jsonEncode({'name': data.name, 'password': data.password}));
+    if (response.statusCode == 200) {
+      globals.userId = jsonDecode(response.body)['id'];
+      return 'Success';
+    } else if (response.statusCode == 401) {
+      return 'Failed';
+    } else {
+      return 'Network Error';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +34,8 @@ class AccountSettingScreen extends StatelessWidget {
         ),
         body: Center(
           child: FlutterLogin(
-            onLogin: (_) => Future(() => null),
+            // onLogin: (_) => Future(() => null),
+            onLogin: _authUser,
             onRecoverPassword: (_) => Future(() => null),
             hideForgotPasswordButton: true,
             onSignup: (_) => Future(() => null),
